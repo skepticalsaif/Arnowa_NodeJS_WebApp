6af0945fc8dc2bfdd21d9b4c31f13542d0f4ea5c
Saif Nizami <saifnizami.official@gmail.com>
2021-08-21 23:04:26 +0530

    added to github
    Signed-off-by: Saif Nizami <saifnizami.official@gmail.com>

--- db.js
+++ db.js
@@ -1,18 +1,27 @@
 const Sequelize = require("sequelize");
 
+// below server is used to deploy the webapp to heroku
+
+// const db = new Sequelize({
+//   dialect: "postgres",
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false,
+//     },
+//   },
+//   database: "d6icjasd4plnf5",
+//   port: "5432",
+//   username: "wxufkowzoppawm",
+//   password: "139b2b7a840edef16227c5e2992e81963361b941e3148dffe5b7b275c55464fd",
+//   host: "ec2-54-243-92-68.compute-1.amazonaws.com",
+// });
+
 const db = new Sequelize({
-  dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-  database: "d6icjasd4plnf5",
-  port: "5432",
-  username: "wxufkowzoppawm",
-  password: "139b2b7a840edef16227c5e2992e81963361b941e3148dffe5b7b275c55464fd",
-  host: "ec2-54-243-92-68.compute-1.amazonaws.com",
+  dialect: "mysql",
+  database: "authlocaldb",
+  username: "authlocaluser",
+  password: "authlocalpass",
 });
 
 const Users = db.define("user", {
